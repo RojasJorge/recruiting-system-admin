d8668a71dafd9ab60cb7648129980af049fad1c0
Jorge Rojas <jorge@royalestudios.com>
2020-02-10 10:12:56 -0600

    UPDATE Remove less loader.

--- next.config.js
+++ next.config.js
@@ -1,22 +1,22 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
 const withImages = require('next-images');
-const withLess = require('@zeit/next-less')
-const lessToJS = require('less-vars-to-js')
-const fs = require('fs');
-const path = require('path');
+// const withLess = require('@zeit/next-less')
+// const lessToJS = require('less-vars-to-js')
+// const fs = require('fs');
+// const path = require('path');
 
-const themeVariables = lessToJS(
-	fs.readFileSync(path.resolve(__dirname, './assets/css/antd-custom.less'), 'utf8')
-);
+// const themeVariables = lessToJS(
+// 	fs.readFileSync(path.resolve(__dirname, './assets/css/antd-custom.less'), 'utf8')
+// );
 
 /** fix: prevents error when .less files are required by node */
-if (typeof require !== 'undefined') {
-	require.extensions['.less'] = file => {}
-}
+// if (typeof require !== 'undefined') {
+// 	require.extensions['.less'] = file => {}
+// }
 
 module.exports =
-	withLess(
+	// withLess(
 		withCSS(
 			withSass(
 				withImages({
@@ -26,11 +26,11 @@ module.exports =
 						API_URL: 'http://localhost:3026/api/v1',
 						// API_URL: 'http://128.199.162.22:3026/api/v1'
 					},
-					lessLoaderOptions: {
-						javascriptEnabled: true,
-						modifyVars: themeVariables // make your antd custom effective
-					},
-					esModule: true,
+					// lessLoaderOptions: {
+					// 	javascriptEnabled: true,
+					// 	modifyVars: themeVariables // make your antd custom effective
+					// },
+					// esModule: true,
 					webpack(config, options) {
 						config.module.rules.push({
 							test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
@@ -46,4 +46,4 @@ module.exports =
 				})
 			)
 		)
-	);
+	// );
