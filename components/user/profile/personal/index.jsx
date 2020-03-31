98d278a93d084dad6d6535774c1072541003170d
Jorge Rojas <jorge@royalestudios.com>
2020-03-30 19:29:53 -0600

    UPDATE Change card component.

--- components/user/profile/personal/index.jsx
+++ components/user/profile/personal/index.jsx
@@ -1,33 +1,32 @@
 import { Card } from "antd";
 import { useStoreState } from "easy-peasy";
-// import Phones from "./Phones";
-import Form from "./Form";
+import PersonalForm from "./Form";
 
 const Personal = ({ update }) => {
   /** Global state */
-  const personal = useStoreState(state => state.profile.steps.personal);
+  // const personal = useStoreState(state => state.profile.steps.personal);
   const user = useStoreState(state => state.auth.user);
 
   return (
     <>
       <h2>Información personal:</h2>
-      <Card title={null}>
-        <h4>Nombre:</h4>
-        <p>
-          {user.name} {user.lastname}
-        </p>
-        <h4>Dirección:</h4>
-        <p>{user.address}</p>
-        <h4>Email:</h4>
-        <p>{user.email}</p>
-        {personal.phones.map(phone => (
-          <li key={phone.id}>
-            {`+${phone.area}`} {phone.number} {phone.type}
-          </li>
-        ))}
-        <Form {...update} />
+      {/*<Card title={null} bordered={false}>*/}
+        {/*<h4>Nombre:</h4>*/}
+        {/*<p>*/}
+        {/*  {user.name} {user.lastname}*/}
+        {/*</p>*/}
+        {/*<h4>Dirección:</h4>*/}
+        {/*<p>{user.address}</p>*/}
+        {/*<h4>Email:</h4>*/}
+        {/*<p>{user.email}</p>*/}
+        {/*{personal.phones.map(phone => (*/}
+        {/*  <li key={phone.id}>*/}
+        {/*    {`+${phone.area}`} {phone.number} {phone.type}*/}
+        {/*  </li>*/}
+        {/*))}*/}
+        <PersonalForm {...update} />
         {/* <Phones update={update} /> */}
-      </Card>
+      {/*</Card>*/}
     </>
   );
 };
