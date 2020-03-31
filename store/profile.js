4ac9b396d516b5173ac7b93d9463d2f92ec8ea80
Jorge Rojas <jorge@royalestudios.com>
2020-03-30 19:19:59 -0600

    UPDATE Add the profile store module.

--- store/profile.js
+++ store/profile.js
@@ -1,29 +1,28 @@
 import {
-  action
+	action
 } from "easy-peasy";
 
 const profile = {
-  steps: {
-    academic: {},
-    documents: {},
-    economic: {},
-    lookingFor: {},
-    others: {},
-    personal: {
-      name: "",
-      lastname: "",
-      phones: [],
-    },
-    working: {}
-  },
-
-  /** Profile updater */
-  update: action((state, payload) => {
-    state.steps = {
-      ...state.steps,
-      [payload.field]: payload.value
-    }
-  })
+	steps: {
+		academic: {},
+		documents: {},
+		economic: {},
+		lookingFor: {},
+		others: {},
+		personal: {
+			name: "",
+			lastname: "",
+			phones: [],
+		},
+		working: {}
+	},
+	
+	/** Profile updater */
+	update: action((state, payload) => state.steps = {
+			...state.steps,
+			[payload.field]: payload.value
+		}
+	)
 };
 
 export default profile;
