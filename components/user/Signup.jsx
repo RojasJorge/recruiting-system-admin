ef3e245c31255e3ac5c605822ee29298626742e6
Jorge Rojas <jorge@royalestudios.com>
2020-03-30 19:44:41 -0600

    UPDATE Change to new antd form validations.

--- components/user/Signup.jsx
+++ components/user/Signup.jsx
@@ -1,9 +1,34 @@
 import Head from "next/head";
 import { Form, Input, Button, DatePicker } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import Router from "next/router";
+import "cleave.js/dist/addons/cleave-phone.gt";
+import Cleave from "cleave.js/react";
 import "animate.css";
 
-const SignupForm = () => {
+const SignupForm = _ => {
+  /** Submit handler */
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(
+      e.target.name.value,
+      e.target.lastname.value,
+      e.target.email.value,
+      e.target.phone.value,
+      e.target.birthday.value,
+      "password",
+      e.target.password.value
+    );
+  };
+
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <>
       <Head>
@@ -17,32 +42,41 @@ const SignupForm = () => {
                 <h1>Signup:</h1>
               </div>
             </div>
-            <Form className="signup--form">
+            <Form 
+            name="basic"
+              className="signup--form" 
+              onSubmit={onSubmit} 
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}>
               <div className="row">
-                <div className="col">
+                <div className="col-md-6">
                   <Form.Item label="Email">
                     <Input
                       name="email"
                       size="large"
                       value="jorge@royalestudios.com"
-                      required
+                      rules={[{ required: true, message: 'Please input your email!' }]}
                     />
                   </Form.Item>
+                </div>
+                <div className="col-md-6">
                   <Form.Item label="Password">
                     <Input.Password
                       name="password"
                       size="large"
                       value="royale123"
-                      required
+                      rules={[{ required: true, message: 'Please input your password!' }]}
                     />
                   </Form.Item>
                 </div>
-                <div className="col">
+                <div className="col-md-6">
                   <Form.Item label="Name">
-                    <Input name="name" size="large" required />
+                    <Input name="name" size="large" rules={[{ required: true, message: 'Please input your username!' }]} />
                   </Form.Item>
+                </div>
+                <div className="col-md-6">
                   <Form.Item label="Lastname">
-                    <Input name="lastname" size="large" required />
+                    <Input name="lastname" size="large" />
                   </Form.Item>
                 </div>
                 <div className="col-md-12">
@@ -50,28 +84,29 @@ const SignupForm = () => {
                 </div>
                 <div className="col">
                   <Form.Item label="Phone(s)">
-                    <Input
+                    <Cleave
+                      className="ant-input ant-input-lg"
+                      options={{
+                        phone: true,
+                        phoneRegionCode: "GT"
+                      }}
                       name="phone"
-                      size="large"
-                      placeholder="Eg. 0000-0000, 54343212"
-                      required
+                      onChange={e => console.log(e.target.value)}
                     />
                   </Form.Item>
                 </div>
                 <div className="col">
                   <Form.Item label="Birthday">
-                    <DatePicker style={{ width: "100%" }} size="large" />
+                    <DatePicker
+                      style={{ width: "100%" }}
+                      format="DD/MM/YYYY"
+                      size="large"
+                      name="birthday"
+                    />
                   </Form.Item>
                 </div>
               </div>
-              <Button
-                htmlType="submit"
-                icon="plus"
-                size="large"
-                type="default"
-                // loading={loading}
-                ghost
-              >
+              <Button htmlType="submit" icon={<PlusOutlined />} size="large" type="primary">
                 Register new user
               </Button>
               <Button
