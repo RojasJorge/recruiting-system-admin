84c127e04805d362773b40162763be10de44cb16
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-05-20 21:19:04 -0600

    UPDATE Main wrapper

---
+++ pages/_app.jsx
@@ -0,0 +1,19 @@
+import { StoreProvider } from 'easy-peasy'
+import { ConfigProvider } from 'antd'
+import { AbilityContext } from '../components/Can'
+import store from '../store'
+import ability from '../ability'
+import esEs from 'antd/lib/locale-provider/es_ES'
+import '../assets/css/global.scss'
+
+const WebApp = ({ Component, pageProps }) => (
+  <StoreProvider store={store}>
+    <AbilityContext.Provider value={ability}>
+      <ConfigProvider locale={esEs}>
+        <Component {...pageProps} />
+      </ConfigProvider>
+    </AbilityContext.Provider>
+  </StoreProvider>
+)
+
+export default WebApp
