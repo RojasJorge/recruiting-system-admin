b32d53afa4493ca5056a277edb1c3563e7b8b9a1
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-08-01 17:34:44 -0600

    UPDATE Redirects if token exists

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,7 +1,29 @@
+import Router from 'next/router'
 import Login from '../components/user/Login'
+import {useEffect, useState} from 'react'
 
-const LoginPage = _ => (
-  <Login />
-)
+const LoginPage = _ => {
+	
+	const [loading, setLoading] = useState(true)
+	
+	useEffect(() => {
+		if (localStorage.getItem('uToken')) Router.replace('/admin')
+		setTimeout(() => {
+			setLoading(false)
+		}, 1000)
+	}, [])
+	
+	return <Login />
+	// if (loading) return (
+	// 	<>
+	// 		<p>Verificando...</p>
+	// 		<style jsx>{`
+	// 		text-align: center;
+	// 	`}</style>
+	// 	</>
+	// )
+	//
+	// if(!loading) return <Login />
+}
 
 export default LoginPage
