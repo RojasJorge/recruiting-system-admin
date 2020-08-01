edb9a90ca8c8ce1298d3400b134e057abf4ec9d8
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-08-01 17:32:06 -0600

    FIX Pager state

--- components/user/List.jsx
+++ components/user/List.jsx
@@ -1,93 +1,97 @@
-import { useEffect, useState } from 'react'
-import { useStoreState, useStoreActions } from 'easy-peasy'
-import { Table, Pagination } from 'antd'
+import {useEffect, useState} from 'react'
+import {useStoreState, useStoreActions} from 'easy-peasy'
+import {Table, Pagination} from 'antd'
 import xhr from '../../xhr'
 
 const UsersList = () => {
-  const users = useStoreState(state => state.users)
-  const fill = useStoreActions(actions => actions.users.fill)
-
-  const [pager, updatePager] = useState({
-    page: 1,
-    limit: 10
-  })
-
-  const onChange = async (page, limit) => {
-    updatePager({ page, limit })
-    await get({ page, limit })
-  }
-
-  const get = async p => xhr()
-    .get(`/user?pager=${JSON.stringify(p ? p : pager)}`)
-    .then(res => {
-      res.type = true /** This param (if true) loads a collection, false => single object */
-      fill(res)
-    })
-    .catch(console.error)
-
-  useEffect(() => {
-    get()
-  }, [])
-
-  return (
-    <div className="row">
-      <div className="col-md-12">
-        <h1>Users list</h1>
-      </div>
-      <div className="col-md-12" style={{ marginBottom: 20 }}>
-        <Pagination
-          total={users.total}
-          onChange={onChange}
-          onShowSizeChange={onChange}
-          showSizeChanger
-          pageSizeOptions={["10", "20", "40", "60", "100"]}
-          hideOnSinglePage
-        />
-      </div>
-      <div className="col-md-12">
-        <Table
-          bordered
-          size="middle"
-          dataSource={users.list}
-          rowKey={record => record.id}
-          pagination={false}
-          columns={[
-            {
-              title: "Nombre",
-              dataIndex: "name",
-              key: "name"
-            },
-            {
-              title: "Apellido",
-              dataIndex: "lastname",
-              key: "lastname"
-            },
-            {
-              title: "Dirección",
-              dataIndex: "address",
-              key: "address"
-            },
-            {
-              title: "Email",
-              dataIndex: "email",
-              key: "email"
-            },
-            {
-              title: "Teléfono",
-              dataIndex: "phone",
-              key: "phone"
-            },
-            {
-              title: "Estado",
-              dataIndex: "status",
-              key: "status",
-              render: s => (s ? "Activo" : "Inactivo")
-            }
-          ]}
-        />
-      </div>
-    </div>
-  )
+	const users = useStoreState(state => state.users)
+	const fill = useStoreActions(actions => actions.users.fill)
+	
+	const [pager, updatePager] = useState({
+		page: 1,
+		limit: 10
+	})
+	
+	const onChange = async (page, limit) => {
+		await get(page, limit)
+		updatePager({...pager, page, limit})
+	}
+	
+	const get = async (page, limit) => xhr()
+		.get(`/user?page=${page}&offset=${limit}`)
+		.then(res => {
+			res.type = true
+			/** This param (if true) loads a collection, false => single object */
+			fill(res)
+		})
+		.catch(console.error)
+	
+	useEffect(() => {
+		get(pager.page, pager.limit)
+	}, [])
+	
+	return (
+		<div className="row">
+			<div className="col-md-12">
+				<h1>Users list</h1>
+			</div>
+			{/*<div>*/}
+			{/*  <pre>{JSON.stringify(users, false, 2)}</pre>*/}
+			{/*</div>*/}
+			<div className="col-md-12" style={{marginBottom: 20}}>
+				<Pagination
+					total={users.total}
+					current={pager.page}
+					onChange={onChange}
+					onShowSizeChange={onChange}
+					showSizeChanger
+					pageSizeOptions={["10", "20", "40", "60", "100"]}
+				/>
+			</div>
+			<div className="col-md-12">
+				<Table
+					bordered
+					size="middle"
+					dataSource={users.list}
+					rowKey={record => record.id}
+					pagination={false}
+					columns={[
+						{
+							title: "Nombre",
+							dataIndex: "name",
+							key: "name"
+						},
+						{
+							title: "Apellido",
+							dataIndex: "lastname",
+							key: "lastname"
+						},
+						{
+							title: "Dirección",
+							dataIndex: "address",
+							key: "address"
+						},
+						{
+							title: "Email",
+							dataIndex: "email",
+							key: "email"
+						},
+						{
+							title: "Teléfono",
+							dataIndex: "phone",
+							key: "phone"
+						},
+						{
+							title: "Estado",
+							dataIndex: "status",
+							key: "status",
+							render: s => (s ? "Activo" : "Inactivo")
+						}
+					]}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default UsersList
