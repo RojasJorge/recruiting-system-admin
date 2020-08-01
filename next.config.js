af741b25a54943c14dc47499e98d6b9223a3bf3d
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-08-01 17:26:35 -0600

    UPDATE scss, images features

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-// next.config.js
-const withSass = require('@zeit/next-sass');
-const withImages = require('next-images');
+const path = require('path')
+const withImages = require('next-images')
 
 module.exports = withImages({
-  env: {
-    APP_TITLE: ' | Umana admin',
-    APP_CLOUDINARY: 'https://api.cloudinary.com/v1_1/umana-storage',
-    APP_ENV: 'development',
-    // API_URL: 'http://localhost:3026/api/v1',
-    // API_URL: 'https://staging.umana.co:3026/api/v1',
-  },
-});
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'assets/scss')],
+	},
+})
