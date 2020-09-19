c88393f38be1f95106dafaba3994b13c18c5c4e2
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-09-18 19:01:43 -0600

    UPDATE Add edit link view

--- pages/admin/profile/index.jsx
+++ pages/admin/profile/index.jsx
@@ -1,11 +1,21 @@
 import Layout from '../../../views/Layout';
 import UserProfile from '../../../components/user/profile';
+import {EditOutlined} from '@ant-design/icons'
 import {Empty} from 'antd'
+import Link from 'next/link'
+import router from 'next/router'
 
 const Index = _ => (
   <Layout title="Mi perfil" className="profile">
     <div className="row">
       <div className="col">
+        <Link href="/admin/profile/edit?current=0" passHref>
+          <a>
+            <EditOutlined /> Editar
+          </a>
+        </Link>
+      </div>
+      <div className="col-md-12">
         <Empty/>
       </div>
     </div>
