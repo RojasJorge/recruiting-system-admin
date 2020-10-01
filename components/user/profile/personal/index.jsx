2102b474a6d77ccf41d736eb4266752b22e76613
Andrea España <sae.jacky96@gmail.com>
2020-10-01 02:46:36 -0600

    ADD styles to profile form

--- components/user/profile/personal/index.jsx
+++ components/user/profile/personal/index.jsx
@@ -1,111 +1,95 @@
-import {useState} from "react";
-import {useStoreActions, useStoreState} from "easy-peasy";
-import {Button, Form, notification} from "antd";
-import Names from "./Names";
-import router from 'next/router'
-import Locations from "../../../Location";
-import General from "./General";
-import Contact from "./Contact";
-import {DoubleRightOutlined} from "@ant-design/icons";
-import xhr from "../../../../xhr";
+import { useState } from 'react';
+import { useStoreActions, useStoreState } from 'easy-peasy';
+import { Button, Form, notification } from 'antd';
+import Names from './Names';
+import router from 'next/router';
+import Locations from '../../../Location';
+import General from './General';
+import Contact from './Contact';
+import { DoubleRightOutlined } from '@ant-design/icons';
+import xhr from '../../../../xhr';
 
 /** Import form sections */
 const FormItem = Form.Item;
 
-const Personal = ({switchCurrent, current}) => {
-	/** Global state */
-	const {
-		profile: {
-			id,
-			fields: {
-				personal
-			}
-		},
-		
-	} = useStoreState(state => state.auth.user)
-	
-	/** Birthday handler */
-	const [birthday, setBirthday] = useState(personal.birthday)
-	
-	/** Danger status */
-	const [danger, isDanger] = useState(false)
-	
-	/** Phones */
-	const [phones, setPhones] = useState([])
-	
-	/** Personal info */
-	const updateProfile = useStoreActions(actions => actions.auth.updateProfile)
-	
-	const onFinish = fields => {
-		fields = {...fields, birthday}
-		
-		xhr()
-			.put(`/profile/${id}`, JSON.stringify({
-				fields: {
-					personal: fields
-				}
-			}))
-			.then(resp => {
-				updateProfile({type: 'personal', fields})
-				
-				/** Send notification success */
-				notify('success', 'Ficha personal actualizada.', 'Vamos al siguiente paso...')
-				switchCurrent((current + 1))
-				router.push(`${router.router.pathname}?current=${(current + 1)}`)
-			})
-			.catch(err => console.log('Error:', err))
-	}
-	
-	/** Notifications */
-	const notify = (type, message, description) => {
-		notification[type]({
-			message,
-			description
-		})
-	}
-	
-	return (
-		<>
-			{/*<pre>{JSON.stringify(personal, false, 2)}</pre>*/}
-			<div className="row">
-				<div className="col-md-12">
-					<h2>Información personal:</h2>
-				</div>
-			</div>
-			<Form
-				name="basic"
-				className="row"
-				onFinish={onFinish}
-				initialValues={personal}
-			>
-				<Names/>
-				<Form.Item
-					name="location"
-				>
-					<Locations/>
-				</Form.Item>
-				{/*<Salary/>*/}
-				<General
-					birthday={birthday}
-					setBirthday={setBirthday}
-				/>
-				<Contact
-					phones={phones}
-					setPhones={setPhones}
-				/>
-				<div className="col-md-12">
-					<FormItem>
-						<Button
-							type="primary"
-							htmlType="submit"
-							icon={<DoubleRightOutlined/>}
-							size="large"
-							danger={danger}>Confirmar y continuar</Button>
-					</FormItem>
-				</div>
-			</Form>
-		</>
-	)
-}
+const Personal = ({ switchCurrent, current }) => {
+  /** Global state */
+  const {
+    profile: {
+      id,
+      fields: { personal },
+    },
+  } = useStoreState(state => state.auth.user);
 
-export default Personal
+  /** Birthday handler */
+  const [birthday, setBirthday] = useState(personal.birthday);
+
+  /** Danger status */
+  const [danger, isDanger] = useState(false);
+
+  /** Phones */
+  const [phones, setPhones] = useState([]);
+
+  /** Personal info */
+  const updateProfile = useStoreActions(actions => actions.auth.updateProfile);
+
+  const onFinish = fields => {
+    fields = { ...fields, birthday };
+
+    xhr()
+      .put(
+        `/profile/${id}`,
+        JSON.stringify({
+          fields: {
+            personal: fields,
+          },
+        }),
+      )
+      .then(resp => {
+        updateProfile({ type: 'personal', fields });
+
+        /** Send notification success */
+        notify('success', 'Ficha personal actualizada.', 'Vamos al siguiente paso...');
+        switchCurrent(current + 1);
+        router.push(`${router.router.pathname}?current=${current + 1}`);
+      })
+      .catch(err => console.log('Error:', err));
+  };
+
+  /** Notifications */
+  const notify = (type, message, description) => {
+    notification[type]({
+      message,
+      description,
+    });
+  };
+
+  return (
+    <>
+      <Form name="basic" onFinish={onFinish} initialValues={personal}>
+        <h2 style={{ width: '100%' }}>Información personal</h2>
+        <Names />
+        <Form.Item name="location" label="Ubicación actual">
+          <Locations />
+        </Form.Item>
+        {/*<Salary/>*/}
+        <General birthday={birthday} setBirthday={setBirthday} />
+        <Contact phones={phones} setPhones={setPhones} />
+
+        <FormItem>
+          <Button
+            type="orange"
+            htmlType="submit"
+            size="large"
+            danger={danger}
+            style={{ width: '100%' }}
+          >
+            Confirmar y continuar
+          </Button>
+        </FormItem>
+      </Form>
+    </>
+  );
+};
+
+export default Personal;
