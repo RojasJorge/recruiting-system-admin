8105acced4176e96f064e276518390e36d8b75d1
Andrea España <sae.jacky96@gmail.com>
2020-09-30 23:52:52 -0600

    FIX single job public

--- pages/jobs/single/[id].jsx
+++ pages/jobs/single/[id].jsx
@@ -1,6 +1,6 @@
-import Guest from '../../views/Guest';
-import { PageTitle } from '../../elements';
-import SingleJob from '../../components/jobs/Single';
+import Guest from '../../../views/Guest';
+import { PageTitle } from '../../../elements';
+import SingleJob from '../../../components/jobs/Singlegle';
 
 const Index = _ => {
   return (
