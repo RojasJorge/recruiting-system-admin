2bd568c46eb300d8cbce3df34566554e21f84fba
Andrea España <sae.jacky96@gmail.com>
2020-10-04 19:10:31 -0600

    UPDATE profile

--- components/user/profile/Economic/Salary.jsx
+++ components/user/profile/Economic/Salary.jsx
@@ -1,73 +1,69 @@
-import {useState} from "react";
-import {Input, InputNumber, Select} from "antd";
+import { useState } from 'react';
+import { Input, InputNumber, Select } from 'antd';
 
-const {Option} = Select
+const { Option } = Select;
 
-const Salary = ({value = {}, onChange}) => {
-	
-	const initVal = Object.keys(value).length > 0
-		? value
-		: {
-			currency: '',
-			baseMin: 0,
-			baseMax: 0
-		}
-	
-	const [values, setValues] = useState(initVal)
-	
-	const triggerOnChange = (val) => {
-		if (onChange) {
-			onChange({
-				...values,
-				...val
-			})
-		}
-	}
-	
-	const handleChange = (e, type) => {
-		setValues({
-			...values,
-			[type]: e
-		})
-		
-		triggerOnChange({
-			[type]: e
-		})
-	}
-	
-	return (
-		<>
-			<div className="col-md-6">
-				<label htmlFor="currency">Moneda</label>
-				<Select
-					onChange={e => handleChange(e, 'currency')}
-					value={values.currency}
-					size="large"
-				>
-					<Option value="GTQ">Quetzal</Option>
-					<Option value="USD">Dólar</Option>
-				</Select>
-			</div>
-			<div className="col-md-6">
-				<label htmlFor="baseMin">Mínimo deseado</label>
-				<InputNumber
-					onChange={e => handleChange(e, 'baseMin')}
-					value={values.baseMin}
-					min={0}
-					size="large"
-				/>
-			</div>
-			<div className="col-md-6">
-				<label htmlFor="baseMax">Máximo deseado</label>
-				<InputNumber
-					onChange={e => handleChange(e, 'baseMax')}
-					value={values.baseMax}
-					min={0}
-					size="large"
-				/>
-			</div>
-		</>
-	)
-}
+const Salary = ({ value = {}, onChange }) => {
+  const initVal =
+    Object.keys(value).length > 0
+      ? value
+      : {
+          currency: '',
+          baseMin: 0,
+          baseMax: 0,
+        };
 
-export default Salary
+  const [values, setValues] = useState(initVal);
+
+  const triggerOnChange = val => {
+    if (onChange) {
+      onChange({
+        ...values,
+        ...val,
+      });
+    }
+  };
+
+  const handleChange = (e, type) => {
+    setValues({
+      ...values,
+      [type]: e,
+    });
+
+    triggerOnChange({
+      [type]: e,
+    });
+  };
+
+  return (
+    <div className="umana-form--group">
+      <div className="form-item--sm">
+        <label htmlFor="currency">Moneda</label>
+        <Select onChange={e => handleChange(e, 'currency')} value={values.currency} size="large">
+          <Option value="GTQ">Quetzal</Option>
+          <Option value="USD">Dólar</Option>
+        </Select>
+      </div>
+      <div className="form-item--sm">
+        <label htmlFor="baseMin">Mínimo deseado</label>
+        <InputNumber
+          onChange={e => handleChange(e, 'baseMin')}
+          value={values.baseMin}
+          min={0}
+          size="large"
+        />
+      </div>
+      <div className="form-item--sm">
+        <label htmlFor="baseMax">Máximo deseado</label>
+        <InputNumber
+          onChange={e => handleChange(e, 'baseMax')}
+          value={values.baseMax}
+          min={0}
+          size="large"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Salary;
