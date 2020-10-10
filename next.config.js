f0e3f9856b453490f501c99bd31924016f6fed5e
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-10-10 13:44:18 -0600

    UPDATE test vercel config for cors

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@ const path = require('path')
 const withImages = require('next-images')
 
 module.exports = withImages({
+	async headers() {
+		return [
+			{
+				// mathching all API routes
+				source: "/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+					{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+				]
+			}
+		]
+	},
 	sassOptions: {
 		includePaths: [path.join(__dirname, 'assets/scss')],
 	},
