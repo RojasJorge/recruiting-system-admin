637744c2d2cbe5ffdf68ed07d5624de78c9fa990
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-10-13 17:38:32 -0600

    FIX Remove param from uploader

--- components/Misc/AvatarCropper.jsx
+++ components/Misc/AvatarCropper.jsx
@@ -93,7 +93,7 @@ const AvatarCropper = () => {
 					onChange={onChange}
 					onPreview={onPreview}
 					onRemove={onRemove}
-					multiple={false}
+					// multiple={false}
 				>
 					{fileList.length < 1 && 'Seleccionar'}
 				</Upload>
