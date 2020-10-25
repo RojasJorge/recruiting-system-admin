b803b5f7ce256e6bfd816511aa5aa041382fbe0c
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-10-24 22:08:39 -0600

    UPDATE single profile view

--- components/user/single/index.jsx
+++ components/user/single/index.jsx
@@ -1,45 +1,27 @@
-import General from './general';
-import Contact from './contact';
-import About from './about';
-import Experience from './experience';
-import Academic from './academic';
-import Knowledge from './knowledge';
-import Economic from './economic';
+import General from './general'
+import Contact from './contact'
+import About from './about'
+import Experience from './experience'
+import Academic from './academic'
+import Knowledge from './knowledge'
+import Economic from './economic'
+import {useStoreState} from "easy-peasy"
 
-const SingleProfile = props => {
+const SingleProfile = _ => {
   
-  
-  
-  const getLocal = field => {
-    if (props.data) {
-      const local = props.data.profile;
-      return local.fields[field];
-    } else {
-      const local = JSON.parse(localStorage.getItem('uUser'));
-      return local.profile.fields[field];
-    }
-  };
-  const getUser = field => {
-    if (props.data) {
-      const local = props.data;
-      return local;
-    } else {
-      const local = JSON.parse(localStorage.getItem('uUser'));
-      return local;
-    }
-  };
+  const user = useStoreState(state => state.auth.user)
 
   return (
     <>
-      <General data={getLocal('personal')} defaultData={getUser()} />
-      <Contact data={getLocal('personal')} defaultData={getUser()} />
-      <About data={getLocal('lookingFor')} defaultData={getLocal('personal')} />
-      <Experience data={getLocal('working')} />
-      <Academic data={getLocal('academic')} />
-      <Knowledge data={getLocal('others')} />
-      <Economic data={getLocal('economic')} />
+      <General data={user.profile.fields.personal} defaultData={user} />
+      <Contact data={user.profile.fields.personal} defaultData={user} />
+      <About data={user.profile.fields.lookingFor} defaultData={user.profile.fields.personal} />
+      <Experience data={user.profile.fields.working} />
+      <Academic data={user.profile.fields.academic} />
+      <Knowledge data={user.profile.fields.others} />
+      <Economic data={user.profile.fields.economic} />
     </>
-  );
-};
+  )
+}
 
-export default SingleProfile;
+export default SingleProfile
