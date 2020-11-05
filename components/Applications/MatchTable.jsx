b32b4f972897978fc22b69e3c03919104ff60e09
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-11-04 18:32:01 -0600

    UPDATE Add match score on candidates table

--- components/Applications/MatchTable.jsx
+++ components/Applications/MatchTable.jsx
@@ -1,12 +1,13 @@
 import {Table} from 'antd'
 import PropTypes from 'prop-types'
-import {useStoreState} from "easy-peasy";
+import {useStoreActions, useStoreState} from "easy-peasy";
 import {MailOutlined} from '@ant-design/icons'
 import {find} from 'lodash'
 
 const MatchTable = ({data}) => {
 	
 	const collections = useStoreState(state => state.collections)
+	const match = useStoreActions(actions => actions.jobs.match)
 	
 	const currentJobTitle = cuid =>
 		find(collections.career, o => o.id === cuid).name
@@ -22,9 +23,10 @@ const MatchTable = ({data}) => {
 	return (
 		<div style={{marginTop: 30}}>
 			<h2>Coinsidencias</h2>
+			{/*<pre>{JSON.stringify(data, false, 2)}</pre>*/}
 			<Table
 				dataSource={data}
-				rowKey={record => record.id}
+				rowKey={record => record.profile.id}
 				pagination={false}
 				onRow={onRow}
 				bordered
@@ -40,7 +42,6 @@ const MatchTable = ({data}) => {
 					dataIndex: 'profile.fields.personal',
 					key: 'profile.fields.personal',
 					render: (text, record) => <div>
-						{/*{record.profile.fields.personal.currentJobTitle}*/}
 						{currentJobTitle(record.profile.fields.personal.currentJobTitle)}
 					</div>
 				}, {
@@ -50,6 +51,12 @@ const MatchTable = ({data}) => {
 					render: (text, record) => <div>
 						{record.profile.fields.personal.location.city}, {record.profile.fields.personal.location.country}
 					</div>
+				}, {
+					title: 'Match',
+					dataIndex: 'job',
+					key: 'job',
+					render: (text, record) => `${match(record)}%`
+					// render: (text, record) => <pre>{JSON.stringify(match(record), false, 2)}</pre>
 				}, {
 					title: 'Email',
 					dataIndex: 'profile.fields.personal',
@@ -62,7 +69,7 @@ const MatchTable = ({data}) => {
 					dataIndex: 'profile.fields.personal',
 					key: 'profile.fields.personal',
 					render: (text, record) => <div>
-						<MailOutlined style={{fontSize: 20}} />
+						<MailOutlined style={{fontSize: 20}}/>
 					</div>
 				}]}
 			/>
