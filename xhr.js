395002d3a702d5a0b1ab25331bde5b51386e1913
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-11-04 18:31:36 -0600

    FIX Add redirect on 401 response

--- xhr.js
+++ xhr.js
@@ -1,37 +1,39 @@
 import axios from 'axios';
-import { message } from 'antd';
+import {message} from 'antd';
 import store from "./store";
+import {router} from "next/router";
 
 const xhr = () => {
-  
-  const auth = store.getState().auth
-  
-  const axiosinstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL_PRODUCTION,
-    timeout: 500000,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: auth.token,
-    },
-  });
-
-  axiosinstance.interceptors.response.use(
-    function(response) {
-      return response;
-    },
-    error => {
-      if (error.response && error.response.status === 401) {
-        message.error('La sesión ha expirado');
-        store.persist.clear().then(() => {
-          console.log('Persisted states has been removed')
-        })
-      }
-      // Do something with response error
-      return Promise.reject(error);
-    },
-  );
-
-  return axiosinstance;
+	
+	const auth = store.getState().auth
+	
+	const axiosinstance = axios.create({
+		baseURL: process.env.NEXT_PUBLIC_API_URL_PRODUCTION,
+		timeout: 500000,
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: auth.token,
+		},
+	});
+	
+	axiosinstance.interceptors.response.use(
+		function (response) {
+			return response;
+		},
+		error => {
+			if (error.response && error.response.status === 401) {
+				store.persist.clear().then(() => {
+					message.error('La sesión ha expirado');
+					console.log('Persisted states has been removed')
+					router.push('/')
+				})
+			}
+			// Do something with response error
+			return Promise.reject(error);
+		},
+	);
+	
+	return axiosinstance;
 };
 
 export default xhr;
