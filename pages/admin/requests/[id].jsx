a646a2945047be28070ce76096f7dd402822121c
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-11-06 17:16:45 -0600

    FIX Set loader on single request

--- pages/admin/requests/[id].jsx
+++ pages/admin/requests/[id].jsx
@@ -1,38 +1,15 @@
-import {useEffect, useState} from 'react';
-import xhr from '../../../xhr';
-import Layout from '../../../views/Layout';
-import {Empty} from 'antd'
-import Single from '../../../components/Applications/Single';
-import {isEmpty} from 'lodash';
+import Layout from '../../../views/Layout'
+import Single from '../../../components/Applications/Single'
 
 const Index = query => {
 	
-	/** Local state */
-	const [record, updateRecord] = useState({});
-	
-	const getRecord = async _ =>
-		await xhr()
-			.get(`/apply/${query.id}`)
-			.then(resp => updateRecord(resp.data.items ? resp.data.items[0] : {}))
-			.catch(err => console.log(err));
-	
-	useEffect(() => {
-		getRecord()
-	}, [query.id]);
-	
 	return (
 		<Layout title={'Mi aplicación'}>
-			<>
-				{
-					!isEmpty(record)
-						? <Single record={record}/>
-						: <Empty/>
-				}
-			</>
+			<Single query={query}/>
 		</Layout>
 	);
 };
 
 Index.getInitialProps = async ctx => ctx.query
 
-export default Index;
+export default Index
