cbd95be881e5508b2011aaf5af7398891404037e
Andrea España <sae.jacky96@gmail.com>
2020-11-11 12:10:37 -0600

    ADD back button in catalogos

--- pages/admin/catalogs/academic-level.jsx
+++ pages/admin/catalogs/academic-level.jsx
@@ -1,19 +1,17 @@
 import Layout from '../../../views/Layout';
 import { PageTitle } from '../../../elements';
 import List from '../../../components/catalogs/List';
-import Link from 'next/link'
-import {RollbackOutlined} from "@ant-design/icons";
+import Link from 'next/link';
+import { RollbackOutlined } from '@ant-design/icons';
 
 const AcademicLevels = () => {
   return (
     <Layout title="Niveles académicos">
       <>
         <PageTitle title="Niveles académicos" back="/admin/catalogs" />
-        <h3 style={{marginBottom: 30}}>
-          <Link href="/admin/catalogs" passHref>
+        <h3 style={{ marginBottom: 30 }}>{/* <Link href="/admin/catalogs" passHref>
             <a><RollbackOutlined /> Volver</a>
-          </Link>
-        </h3>
+          </Link> */}</h3>
         <List type="academic_level" title="Niveles académicos" />
       </>
     </Layout>
