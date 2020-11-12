8fcd815ce5589b2bbed6514ccbfac1d658819c9d
Andrea España <sae.jacky96@gmail.com>
2020-11-12 11:43:07 -0600

    FIX profile update

--- components/user/profile/index.jsx
+++ components/user/profile/index.jsx
@@ -1,6 +1,6 @@
-import {useEffect, useState} from 'react';
-import {useStoreState} from 'easy-peasy';
-import {Steps} from 'antd';
+import { useEffect, useState } from 'react';
+import { useStoreState } from 'easy-peasy';
+import { Steps } from 'antd';
 import Personal from './personal';
 import Documents from './Documents';
 import About from './personal/About';
@@ -11,161 +11,144 @@ import Economic from './Economic';
 import Others from './Others';
 import Courses from './Courses';
 import router from 'next/router';
-import {PageTitle, Sitebar} from '../../../elements';
-import Dashboard from "./Dashboard";
+import { PageTitle, Sitebar } from '../../../elements';
+import Dashboard from './Dashboard';
 
-const {Step} = Steps;
+const { Step } = Steps;
 
 const checkList = [
-	{
-		title: 'Personal',
-		id: 'personal',
-		icon: 'person',
-	},
-	{
-		title: 'Documentos',
-		id: 'documents',
-		icon: 'attachment',
-	},
-	{
-		title: 'Acerca de',
-		id: 'about',
-		icon: 'contacts',
-	},
-	{
-		title: '¿Qué buscas?',
-		id: 'looking-for',
-		icon: 'search',
-	},
-	{
-		title: 'Niveles Académicos',
-		id: 'academic-levels',
-		icon: 'school',
-	},
-	{
-		title: 'Otros Cursos',
-		id: 'other',
-		icon: 'book',
-	},
-	{
-		title: 'Otros Conocimientos',
-		id: 'other-knowledge',
-		icon: 'collections_bookmark',
-	},
-	{
-		title: 'Experiencia Laboral',
-		id: 'work-experience',
-		icon: 'work',
-	},
-	{
-		title: 'Económica / Legal ',
-		id: 'economic-legal',
-		icon: 'monetization_on',
-	},
+  {
+    title: 'Personal',
+    id: 'personal',
+    icon: 'person',
+  },
+  {
+    title: '¿Qué buscas?',
+    id: 'looking-for',
+    icon: 'search',
+  },
+  {
+    title: 'Niveles Académicos',
+    id: 'academic-levels',
+    icon: 'school',
+  },
+  {
+    title: 'Otros Cursos',
+    id: 'other',
+    icon: 'book',
+  },
+  {
+    title: 'Otros Conocimientos',
+    id: 'other-knowledge',
+    icon: 'collections_bookmark',
+  },
+  {
+    title: 'Experiencia Laboral',
+    id: 'work-experience',
+    icon: 'work',
+  },
+  {
+    title: 'Económica / Legal ',
+    id: 'economic-legal',
+    icon: 'monetization_on',
+  },
 ];
 
-const UserProfile = ({query}) => {
-	/** Global state */
-	const user = useStoreState(state => state.auth.user);
-	
-	/** Local state */
-	const [current, switchCurrent] = useState(parseInt(query.current, 10) || 0);
-	
-	const onChange = o => {
-		router.push(`${router.pathname}?current=${o}`);
-		window.scroll({
-			top: 80,
-			behavior: 'smooth',
-		});
-	};
-	
-	const status = o => {
-		let s = 'wait';
-		if (o === current) {
-			s = 'process';
-		} else if (o !== current && o < current) {
-			s = 'finish';
-		}
-		
-		return s;
-	};
-	
-	const switchStep = _ => {
-		
-		switch (parseInt(query.current, 10)) {
-			case 0:
-				return <Personal/>;
-				break;
-			case 1:
-				return <Documents/>;
-				break;
-			case 2:
-				return <About/>;
-				break;
-			case 3:
-				return <LookingFor/>;
-				break;
-			case 4:
-				return <AcademicLevels/>;
-				break;
-			case 5:
-				return <Courses/>;
-				break;
-			case 6:
-				return <Others/>;
-				break;
-			case 7:
-				return <Experiences/>;
-				break;
-			case 8:
-				return <Economic/>;
-				break;
-			default:
-				return <Personal/>;
-				break;
-		}
-	};
-	
-	const header = {
-		title: user.name + ' ' + user.lastname,
-		icon: 'person',
-		action: 'remove_red_eye',
-		titleAction: 'Ver perfil',
-		urlAction: '/admin/profile',
-	};
-	
-	useEffect(() => {
-		if (query.current) {
-			switchStep()
-			switchCurrent(parseInt(query.current, 10))
-		}
-	}, [query])
-	
-	return (
-		<>
-			<PageTitle title="Mi Perfil" back="/admin/companies"/>
-			<div className="umana-layout-cl">
-				<div className="umana-layout-cl__small ">
-					<Sitebar header={header} theme="orange">
-						<Steps direction="vertical" size="large" current={current} onChange={onChange}>
-							{checkList.map((o, i) => (
-								<Step key={i} title={o.title} status={status(i)} icon={<i className="material-icons">{o.icon}</i>}/>
-							))}
-						</Steps>
-						<Dashboard
-							show={['TotalPercent']}
-							config={{
-								TotalPercent: {
-									legend: false
-								}
-							}}
-						/>
-					</Sitebar>
-				</div>
-				<div className="umana-layout-cl__flex width-section bg-white">{switchStep()}</div>
-			</div>
-		</>
-	);
+const UserProfile = ({ query }) => {
+  /** Global state */
+  const user = useStoreState(state => state.auth.user);
+
+  /** Local state */
+  const [current, switchCurrent] = useState(parseInt(query.current, 10) || 0);
+
+  const onChange = o => {
+    router.push(`${router.pathname}?current=${o}`);
+    window.scroll({
+      top: 80,
+      behavior: 'smooth',
+    });
+  };
+
+  const status = o => {
+    let s = 'wait';
+    if (o === current) {
+      s = 'process';
+    } else if (o !== current && o < current) {
+      s = 'finish';
+    }
+
+    return s;
+  };
+
+  const switchStep = _ => {
+    switch (parseInt(query.current, 10)) {
+      case 0:
+        return <Personal />;
+        break;
+      case 1:
+        return <LookingFor />;
+        break;
+      case 2:
+        return <AcademicLevels />;
+        break;
+      case 3:
+        return <Courses />;
+        break;
+      case 4:
+        return <Others />;
+        break;
+      case 5:
+        return <Experiences />;
+        break;
+      case 6:
+        return <Economic />;
+        break;
+      default:
+        return <Personal />;
+        break;
+    }
+  };
+
+  const header = {
+    title: user.name + ' ' + user.lastname,
+    icon: 'person',
+    action: 'remove_red_eye',
+    titleAction: 'Ver perfil',
+    urlAction: '/admin/profile',
+  };
+
+  useEffect(() => {
+    if (query.current) {
+      switchStep();
+      switchCurrent(parseInt(query.current, 10));
+    }
+  }, [query]);
+
+  return (
+    <>
+      <PageTitle title="Mi Perfil" back="/admin/companies" />
+      <div className="umana-layout-cl">
+        <div className="umana-layout-cl__small ">
+          <Sitebar header={header} theme="orange">
+            <Steps direction="vertical" size="large" current={current} onChange={onChange}>
+              {checkList.map((o, i) => (
+                <Step key={i} title={o.title} status={status(i)} icon={<i className="material-icons">{o.icon}</i>} />
+              ))}
+            </Steps>
+            <Dashboard
+              show={['TotalPercent']}
+              config={{
+                TotalPercent: {
+                  legend: false,
+                },
+              }}
+            />
+          </Sitebar>
+        </div>
+        <div className="umana-layout-cl__flex width-section bg-white">{switchStep()}</div>
+      </div>
+    </>
+  );
 };
 
 export default UserProfile;
--- components/user/profile/personal/About.jsx
+++ components/user/profile/personal/About.jsx
@@ -18,32 +18,6 @@ const About = ({ switchCurrent, current }) => {
   /** Personal info */
   const updateProfile = useStoreActions(actions => actions.auth.updateProfile);
 
-  const onFinish = fields => {
-    const merged = Object.assign(personal, fields);
-
-    xhr()
-      .put(
-        `/profile/${id}`,
-        JSON.stringify({
-          fields: {
-            personal: fields,
-          },
-        }),
-      )
-      .then(resp => {
-        updateProfile({ type: 'personal', fields: merged });
-        window.scroll({
-          top: 80,
-          behavior: 'smooth',
-        });
-        /** Send notification success */
-        notify('success', 'Ficha Acerca de actualizada.', 'Vamos al siguiente paso...');
-
-        router.push(`${router.router.pathname}?current=${parseInt(router.router.query.current, 10) + 1}`);
-      })
-      .catch(err => console.log('Error:', err));
-  };
-
   /** Notifications */
   const notify = (type, message, description) => {
     notification[type]({
@@ -54,19 +28,12 @@ const About = ({ switchCurrent, current }) => {
   };
 
   return (
-    <Form onFinish={onFinish} initialValues={personal} scrollToFirstError={true}>
-      <div className="umana-form--section">
-        <h2>Acerca de ti</h2>
-        <Item name="about" className="form-item--lg" label="Cuentanos acerca de ti y tu experiencia" rules={[{ required: true, message: 'Este campo es requerido.' }]}>
-          <TynyEditor />
-        </Item>
-      </div>
-      <Item>
-        <Button style={{ marginLeft: 'auto' }} type="orange" size="small" htmlType="submit">
-          Guardar y continuar
-        </Button>
+    <div className="umana-form--section">
+      <h2>Acerca de ti</h2>
+      <Item name="about" className="form-item--lg" label="Cuentanos acerca de ti y tu experiencia" rules={[{ required: true, message: 'Este campo es requerido.' }]}>
+        <TynyEditor />
       </Item>
-    </Form>
+    </div>
   );
 };
 
