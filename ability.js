8499ebc1713954b3cca0f147b5e1d6d9eaa6f154
Andrea España <sae.jacky96@gmail.com>
2020-11-24 12:13:57 -0600

    UPDATE area jobs

--- ability.js
+++ ability.js
@@ -1,57 +1,45 @@
-import { defineAbility } from '@casl/ability'
-import store from './store'
+import { defineAbility } from '@casl/ability';
+import store from './store';
 
 const ability = _ =>
   defineAbility((can, cannot) => {
-    
-    let scope = 'guest'
-    const user = store.getState().auth.user
-    
-    if(user) scope =  user.scope[0]
+    let scope = 'guest';
+    const user = store.getState().auth.user;
+
+    if (user) scope = user.scope[0];
 
     switch (scope) {
       case 'umana':
-        can('read', ['USERS', 'CATALOGS', 'SPECIAL_ALERTS', 'PROFILE', 'REQUESTS_MENU'])
-        
-        can('view', [
-          'MAIN_LAYOUT',
-          'LOGIN',
-          'COMPANIES',
-          'JOBS',
-          'REQUESTS_ADMIN_VIEW',
-          'REQUEST_UPDATE',
-          'COMPANY_INFO_BLOCK',
-          'MATCH_SCORE_VIEW',
-          'ALL_MENUS',
-          'CONTACT_CANDIDATE_BTN'
-        ])
-        
-        can('edit', ['USERS', 'JOBS', 'COMPANIES', 'UPDATE_SINGLE_REQUEST'])
-        can('add', ['COMPANIES', 'JOBS'])
-        break
-      case 'company':
-        can('read', ['USERS', 'SPECIAL_ALERTS', 'REQUESTS_MENU'])
-        can('view', ['MAIN_LAYOUT', 'LOGIN', 'REQUESTS_ADMIN_VIEW', 'REQUEST_UPDATE', 'COMPANY_INFO_BLOCK', 'MATCH_SCORE_VIEW', 'CONTACT_CANDIDATE_BTN'])
-        can('edit', ['USERS', 'JOBS', 'COMPANIES', 'UPDATE_SINGLE_REQUEST'])
-        can('add', ['COMPANIES', 'JOBS'])
+        can('read', ['USERS', 'CATALOGS', 'SPECIAL_ALERTS', 'PROFILE', 'REQUESTS_MENU']);
 
-        break
+        can('view', ['MAIN_LAYOUT', 'LOGIN', 'COMPANIES', 'JOBS', 'REQUESTS_ADMIN_VIEW', 'REQUEST_UPDATE', 'COMPANY_INFO_BLOCK', 'MATCH_SCORE_VIEW', 'ALL_MENUS', 'CONTACT_CANDIDATE_BTN', 'ALL_JOBS']);
+
+        can('edit', ['USERS', 'JOBS', 'COMPANIES', 'UPDATE_SINGLE_REQUEST']);
+        can('add', ['COMPANIES', 'JOBS']);
+        break;
+      case 'company':
+        can('read', ['USERS', 'SPECIAL_ALERTS', 'REQUESTS_MENU']);
+        can('view', ['MAIN_LAYOUT', 'LOGIN', 'REQUESTS_ADMIN_VIEW', 'REQUEST_UPDATE', 'COMPANY_INFO_BLOCK', 'MATCH_SCORE_VIEW', 'CONTACT_CANDIDATE_BTN']);
+        can('edit', ['USERS', 'JOBS', 'COMPANIES', 'UPDATE_SINGLE_REQUEST']);
+        can('add', ['COMPANIES', 'JOBS']);
+
+        break;
 
       case 'candidate':
-        can('read', ['USERS', 'REQUESTS_MENU'])
-        can('view', ['MAIN_LAYOUT', 'LOGIN', 'JOBS', 'COMPANIES', 'OWN_REQUESTS', 'COMPANY_INFO_BLOCK'])
-        can('apply', ['JOBS'])
-        cannot('view', ['REQUESTS_ADMIN_VIEW'])
+        can('read', ['USERS', 'REQUESTS_MENU']);
+        can('view', ['MAIN_LAYOUT', 'LOGIN', 'JOBS', 'COMPANIES', 'OWN_REQUESTS', 'COMPANY_INFO_BLOCK', 'ALL_JOBS']);
+        can('apply', ['JOBS']);
+        cannot('view', ['REQUESTS_ADMIN_VIEW']);
 
-        break
+        break;
       case 'guest':
-        can('guest', ['JOBS'])
-        break
+        can('guest', ['JOBS', 'ALL_JOBS']);
+        break;
 
       default:
-        can('view', ['LOGIN'])
-        break
+        can('view', ['LOGIN']);
+        break;
     }
-  })
+  });
 
-export default ability
+export default ability;
