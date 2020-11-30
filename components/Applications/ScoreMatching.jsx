4c229f2a377aae0a4fff6b3378c032f75a9ef965
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-11-29 20:19:53 -0600

    UPDATE table style

--- components/Applications/ScoreMatching.jsx
+++ components/Applications/ScoreMatching.jsx
@@ -107,21 +107,6 @@ const ScoreMatching = ({data}) => {
 		return
 	}
 	
-	// return (
-	// 	<>
-	// 		<div style={{display: 'flex', maxWidth: 1200, backgroundColor: '#ffffff'}}>
-	// 			<div className="col" style={{width: '60%', borderRight: '1px solid grey'}}>
-	// 				<h3>Data:</h3>
-	// 				<pre>{JSON.stringify(data, false, 2)}</pre>
-	// 			</div>
-	// 			<div className="col">
-	// 				<h3>Score:</h3>
-	// 				<pre>{JSON.stringify(score, false, 2)}</pre>
-	// 			</div>
-	// 		</div>
-	// 	</>
-	// )
-	
 	return (
 		<>
 		<div style={{textAlign: 'center'}}>
