4e3fdfda3b127309de68f3286d924151fabfb60a
Andrea España <sae.jacky96@gmail.com>
2020-12-04 10:06:40 -0600

    ADD sitebar users

--- components/user/List.jsx
+++ components/user/List.jsx
@@ -1,16 +1,48 @@
-import {PageTitle} from '../../elements';
+import { PageTitle, Sitebar } from '../../elements';
+import { Steps } from 'antd';
+import { useState } from 'react';
 import ListCandidate from './archive/candidates';
 import ListCompanies from './archive/companies';
 
+const { Step } = Steps;
+
 const UsersList = () => {
-	return (
-		<div className="">
-			<PageTitle title="Lista de usuarios"/>
-			
-			<ListCandidate/>
-			<ListCompanies/>
-		</div>
-	);
+  const [current, setCurrent] = useState(0);
+
+  const switchTabs = _ => {
+    switch (current) {
+      case 0:
+        return <ListCandidate />;
+        break;
+      case 1:
+        return <ListCompanies />;
+        break;
+      default:
+        return <ListCandidate />;
+        break;
+    }
+  };
+
+  const handleSteps = o => {
+    setCurrent(o);
+  };
+
+  return (
+    <div className="">
+      <PageTitle title="Lista de usuarios" />
+      <div className="umana-layout-cl">
+        <div className="umana-layout-cl__small">
+          <Sitebar>
+            <Steps current={current} onChange={handleSteps} direction="vertical">
+              <Step key={0} title="Candidatos" icon={<i className="material-icons">group</i>} />
+              <Step key={1} title="Empresas" icon={<i className="material-icons">business</i>} />
+            </Steps>
+          </Sitebar>
+        </div>
+        <div className="umana-layout-cl__flex">{switchTabs()}</div>
+      </div>
+    </div>
+  );
 };
 
 export default UsersList;
