0fbd26dc1bc46f1ccd01067b5daf9b493ae56257
Jorge Rojas <jorge.alberto.rojas.solorzano@gmail.com>
2020-12-07 22:11:00 -0600

    FIX Vehicle validation

--- components/Applications/ScoreMatching.jsx
+++ components/Applications/ScoreMatching.jsx
@@ -66,7 +66,12 @@ const ScoreMatching = ({data}) => {
 	
 	const checkVehicles = _ => {
 		
-		if (data.job.vehicle[0] === 'indifferent') return []
+		let result = false
+		
+		if (data.job.vehicle.find(o => o === 'indifferent')) {
+			result = true
+		}
+		
 		let vehicle = []
 		
 		vehicle = data.candidate.profile.fields.economic.vehicles.reduce((acc, current) => {
@@ -77,7 +82,11 @@ const ScoreMatching = ({data}) => {
 			return acc
 		}, [])
 		
-		return vehicle
+		if(vehicle.length > 0) {
+			result = true
+		}
+		
+		return result
 	}
 	
 	const checkLicence = _ => {
@@ -309,8 +318,9 @@ const ScoreMatching = ({data}) => {
 						<td>
 							<p>Vehículo</p>
 							{
-								checkVehicles().length > 0 ? <h3 className="success">Aplica</h3> :
-									<h3 className="nosuccess">No aplica</h3>
+								checkVehicles
+									? <h3 className="success">Aplica</h3>
+									: <h3 className="nosuccess">No aplica</h3>
 							}
 						</td>
 						<td>{
